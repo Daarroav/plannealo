9ba86583ef98679115307122c2d2cfc8b86943fb
Jonathan Quistiano <jhonquistiano@gmail.com>
2025-09-14 14:17:41 -0600

    feat(colDocumentos): Se agrega columna de documentos

---
+++ migrations/0002_late_proudstar.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "activities" ADD COLUMN "attachments" text[];--> statement-breakpoint
+ALTER TABLE "cruises" ADD COLUMN "attachments" text[];--> statement-breakpoint
+ALTER TABLE "flights" ADD COLUMN "attachments" text[];--> statement-breakpoint
+ALTER TABLE "transports" ADD COLUMN "attachments" text[];
